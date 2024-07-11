eadfaa7b10be42e9dc23c3a7d7fbf7bf011bc84b
Niall Carroll <niallpcarroll@gmail.com>
2024-07-11 14:23:37 +0000

    Add welcome statement and function for adding date value

--- run.py
+++ run.py
@@ -12,9 +12,30 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('expenses_calculator')
 
-expenses = SHEET.worksheet('expenses')
+def give_date():
+    """
+    Function to prompt user to enter date. Specify date format.
+    Loop to repeat if date format is invalid.
+    """
+    while True:
+        print("Enter date of event.")
+        print("Date should be in the format dd/mm/yyyy")
 
-data = expenses.get_all_values()
-print(data)
+        date = input("Enter date here:\n")
 
+        if valid_data(date):
+            print("Date is correct!")
+            break
+    return date
+
+def valid_data():
+    """
+    Check to ensure that date values are in the correct format.
+    """        
+
+
+def main():
+    date = give_date()
+print("Welcome to your travel expenses calculator! Let's calculate and record your recent expenses.")
+main()
 
