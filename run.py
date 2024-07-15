cf2c99111b386b99d3d57685570345e9c3e242c4
Niall Carroll <niallpcarroll@gmail.com>
2024-07-15 18:20:51 +0000

    Add functions for inputting location, event name, fee and distance travelled. Begin function for calculating travel expenses

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ def give_date():
     print("Date should be in the format dd/mm/yyyy")
 
     date_input = input("Enter date here: \n")
-    print(f"The date you entered is {date_input}")
+    print(f"The date you entered is: {date_input}")
 
 def validate_date(values):
     """
@@ -36,7 +36,52 @@ def validate_date(values):
     except ValueError as e:
         print(f"Invalid date: (e), try again")
   
+def give_location():
+    """
+    Function to prompt user to insert event location.
+    """
+    print("Please enter event location.")
+    location_input = input(f"Enter event location here: \n")
+    print(f"The event location you entered is: {location_input}")
 
+def give_event():
+    """
+    Function to prompt user to insert event name.
+    """  
+    print("Please enter event type.")  
+    print("For example: Wedding, Funeral, etc.")
+    event_input = input(f"Enter event type here: \n")
+    print(f"The event type you entered is: {event_input}")
+
+def give_fee():
+    """
+    Function to prompt user to insert fee amount.
+    """    
+    print("Please enter the fee for this event.")
+    print("Fees should be in Euro (€)")
+    fee_input = input(f"The fee for this event is: € \n")
+    print(f"The fee you have recorded for this event is: €{fee_input}")
+
+def give_travel():
+    """
+    Function to prompt user to insert distance travelled in kilometers.
+    """    
+    print("Please enter the distance travelled for this event.")
+    print("Distance should be given in kilometers")
+    travel_input = input(f"The distance travelled is: (km) \n")
+    print(f"You travelled {travel_input} kms for this event")
+
+def travel_expenses():
+    """
+    Function to calculate travel expenses based on distance travelled.
+    """
+    travel_expenses = int(travel_input) * 0.43
+    print(travel_expenses)    
     
 give_date()
+give_location()
+give_event()
+give_fee()
+give_travel()
+travel_expenses()
 
